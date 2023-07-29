b4eea18dc6288d5bf5e1ccd515984c17ea53f269
kimkmin06 <kimkmin06@gmail.com>
2023-07-29 10:57:31 +0900

    📝 :: event2

---
+++ chapter3/chapter3.5/app.js
@@ -0,0 +1,43 @@
+const h1 = document.querySelector(".hello:first-child h1");
+
+console.log(h1);
+console.dir(h1);
+
+function handleTitleClick() {
+  h1.style.color = "blue";
+  console.log("h1 was clicked!");
+}
+
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here";
+  console.log("mouse is here!");
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
