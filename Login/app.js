6ee895e740e6cf5042263fb2df054e8ddb03dbc6
kimkmin06 <kimkmin06@gmail.com>
2023-07-30 22:57:24 +0900

    🎨 :: 굳2

--- Login/app.js
+++ Login/app.js
@@ -1,13 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.ariaValueMax;
-  loginForm.classList.add("hidden");
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginButton.addEventListener("submit", onLoginSubmit);
