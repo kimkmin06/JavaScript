e2966ad43b25bbbd14ac4f176175a6378079c05c
kimkmin06 <kimkmin06@gmail.com>
2023-08-01 22:28:15 +0900

    WEATHER

--- chapter8/js/weather.js
+++ chapter8/js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = b4665ce526403da3ba725a77a13fb0b5;
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
   console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then(response => response.json())
+  .then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError() {
